2bf94df828429081a2f4be921830fc5087fac6bf
Brandon Carrel <carrel2@illinois.edu>
2014-12-11 14:09:26 -0600

    Update claw.py
    Add a checkFormat() function to try to prevent errors occurring from misformatted claw.conf file. Just checks brackets around command list right now but will update to do a more robust check in the future. Also added a '-c' option to just check the format of the .conf file and give some indication as to what errors there might be.

--- claw.py
+++ claw.py
@@ -6,62 +6,129 @@ import git
 import smtplib
 from email.mime.text import MIMEText
 
-def editFile( configFile ):
+passwordFlag = False
+
+def suppressPasswords( configFile ):
         #Called when script is run with -p flag.
         #Needs to look through the config file generated and remove
         #passwords and other sensitive information. Should be
         #independent of the machine.
 
-repo = git.Repo( "/home/carrel2" )
-index = repo.index
-file = open( 'hostFile', 'r' )
+        pass
 
-fromAddress = file.readline()[:-1]
-toAddress = file.readline()[:-1]
-subject = file.readline()[:-1]
+def checkFormat( output=False ):
+        file = open( 'claw.conf', 'r' )
 
-while file.readline():
-        hosts = file.readline()[:-1].split( ',' )
-        username = file.readline()[:-1]
-        password = file.readline()[:-1]
-        commands = file.readline()[:-1].split( ',' )
+        contents = file.read()
 
-        for host in hosts:
+        leftBracketList = findAll( contents, '{' )
+        rightBracketList = findAll( contents, '}' )
 
-                client = paramiko.SSHClient()
+        if len( leftBracketList ) != len( rightBracketList ):
+                if output:
+                        print "Missing a bracket"
 
-                client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-                client.connect( host, username=username, password=password )
+                return False
 
-                for command in commands:
-                        configFileName = host + "-" + command.replace( " ", "-" )
-                        configFile = open( configFileName, 'w' )
+        for index in range( len( leftBracketList ) - 1 ):
+                if leftBracketList[index + 1] < rightBracketList[index]:
+                        if output:
+                                print "Misplaced left bracket '{'"
 
-                        stdin, stdout, stderr = client.exec_command( command )
+                        return False
 
-                        configFile.write( stdout.read() )
+                if rightBracketList[index + 1] < leftBracketList[index]:
+                        if output:
+                                print "Misplaced right bracket '}'"
 
-                        if len( sys.argv ) == 2 and sys.argv[1] == '-p':
-                                editFile( configFile )
+                        return False
 
-                        configFile.close()
+        file.close()
 
-                        index.add( [configFileName] )
+        if output:
+                print "All good"
 
-                client.close()
+        return True
 
-file.close()
+def findAll( string, substring ):
+        i = 0
+        indexlist = []
+        index = string.find( substring )
 
-diff = repo.git.diff( '--staged' )
+        while i >= 0:
+                if i != 0:
+                        indexlist.append( i )
 
-if diff:
-        index.commit( configFileName )
-        message = MIMEText( diff )
+                i = string.find( substring, i + 1 )
 
-        message['From'] = fromAddress
-        message['To'] = toAddress
-        message['Subject'] = subject
+        return indexlist
 
-        s = smtplib.SMTP( 'localhost' )
-        s.sendmail( fromAddress, [toAddress], message.as_string() )
-        s.quit()
+def clawMachines():
+        repo = git.Repo( "/home/carrel2" )
+        index = repo.index
+        file = open( 'claw.conf', 'r' )
+
+        fromAddress = file.readline()[:-1]
+        toAddress = file.readline()[:-1]
+        subject = file.readline()[:-1]
+
+        while file.readline():
+                hosts = file.readline()[:-1].split( ',' )
+                username = file.readline()[:-1]
+                password = file.readline()[:-1]
+                commands = []
+
+                while file.readline()[:-1] != '}':
+                        commands.append( file.readline()[:-1] )
+
+                for host in hosts:
+
+                        client = paramiko.SSHClient()
+
+                        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+                        client.connect( host, username=username, password=password )
+
+                        for command in commands:
+                                configFileName = host + "-" + command.replace( " ", "-" )
+                                configFile = open( configFileName, 'w' )
+
+                                stdin, stdout, stderr = client.exec_command( command )
+
+                                configFile.write( stdout.read() )
+
+                                if passwordFlag:
+                                        suppressPasswords( configFile )
+
+                                configFile.close()
+
+                                index.add( [configFileName] )
+
+                        client.close()
+
+        file.close()
+
+        diff = repo.git.diff( '--staged' )
+
+        if diff:
+                index.commit( configFileName )
+                message = MIMEText( diff )
+
+                message['From'] = fromAddress
+                message['To'] = toAddress
+                message['Subject'] = subject
+
+                s = smtplib.SMTP( 'localhost' )
+                s.sendmail( fromAddress, [toAddress], message.as_string() )
+                s.quit()
+
+##                     ##
+# End of clawMachines() #
+##                     ##
+
+if '-c' in sys.argv:
+        checkFormat( output=True )
+elif '-p' in sys.argv and checkFormat():
+        passwordFlag = True
+        clawMachines()
+elif checkFormat():
+        clawMachines()
